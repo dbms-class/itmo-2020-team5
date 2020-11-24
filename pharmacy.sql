a077daf54b5380ccb1038bff3fdffed81deb60ea
Maksim Efimov <hapsidra@gmail.com>
2020-11-24 15:07:47 +0000

    поправили замечания

--- pharmacy.sql
+++ pharmacy.sql
@@ -34,7 +34,7 @@ CREATE TABLE Certificate
 CREATE TABLE ActiveSubstance
 (
     id      SERIAL PRIMARY KEY,
-    name    TEXT NOT NULL,
+    name    TEXT NOT NULL UNIQUE,
     formula TEXT NOT NULL
 );
 
@@ -58,11 +58,11 @@ CREATE TABLE Distributor
 (
     id                  SERIAL PRIMARY KEY,
     address             TEXT NOT NULL,
-    bank_account_number TEXT NOT NULL check ( length(bank_account_number) = 20 and bank_account_number like '4%'
+    bank_account_number TEXT NOT NULL UNIQUE check ( length(bank_account_number) = 20 and bank_account_number like '4%'
 ) ,
   contact_first_name  TEXT NOT NULL,
   contact_last_name   TEXT NOT NULL,
-  phone_number        TEXT NOT NULL check ( length(phone_number) = 12 and phone_number like '+7%' )
+  phone_number        TEXT NOT NULL UNIQUE check ( length(phone_number) = 12 and phone_number like '+7%' )
 );
 
 
@@ -73,15 +73,13 @@ CREATE TABLE Warehouse
     address TEXT NOT NULL UNIQUE
 );
 
--- Поставка
+-- Поставка от дистрибьютора в склад
 CREATE TABLE Supply
 (
     id                          SERIAL PRIMARY KEY,
---  pharmacy_id    INT NOT NULL REFERENCES Pharmacy,
-    distributor_id              INT REFERENCES Distributor, -- склад
-    warehouse_id                INT REFERENCES Warehouse,
-    arrival_time                TIME NOT NULL,
-    arrival_date                DATE NOT NULL,
+    distributor_id              INT REFERENCES Distributor, -- поставщик
+    warehouse_id                INT REFERENCES Warehouse,   -- склад
+    arrival_datetime            TIMESTAMP NOT NULL,
     arrival_storekeeper_surname TEXT NOT NULL
 );
 
@@ -92,7 +90,7 @@ CREATE TABLE ShippingBox
     medicine_id             INT    NOT NULL REFERENCES Medicine,
     package_weight_gr       FLOAT  NOT NULL check ( package_weight_gr > 0 ),       -- Вес перевозочной
     release_packaging_count INT    NOT NULL check ( release_packaging_count > 0 ), -- Количество отпускных упаковке в одной перевозочной
-    price_rub               FLOAT  NOT NULL                                        -- закупочная стоимость отпускной
+    price_rub               MONEY  NOT NULL                                        -- закупочная стоимость отпускной
 );
 
 -- Содержание перевозки
@@ -100,16 +98,18 @@ CREATE TABLE SuplyContent
 (
     id                SERIAL PRIMARY KEY,
     supply_id         INT NOT NULL REFERENCES Supply,
-    shippingbox_id    INT NOT NULL REFERENCES ShippingBox,
-    shippingBox_count INT NOT NULL
+    shipping_box_id    INT NOT NULL REFERENCES ShippingBox,
+    shipping_box_count INT NOT NULL,
+    UNIQUE(supply_id, shipping_box_id)
 );
+
 
 -- Аптека
 CREATE TABLE Pharmacy
 (
     id            SERIAL PRIMARY KEY,
     address       TEXT UNIQUE NOT NULL,
-    number_people TEXT UNIQUE
+    name          TEXT UNIQUE
 );
 
 -- Ассортимент
@@ -122,8 +122,6 @@ CREATE TABLE Assortment_pharmacy
     PRIMARY KEY (pharmacy_id, medicine_id)
 );
 
--- Закончили здесь --
-
 -- Автомобили
 CREATE TABLE Cars
 (
@@ -131,35 +129,32 @@ CREATE TABLE Cars
     maintenance DATE NOT NULL
 );
 
-
+-- Поставка в аптеку из склада
 CREATE TABLE Transportation
 (
-    id                    SERIAL REFERENCES Cars,
-    date                  DATE,
+    car_id                INT REFERENCES Cars,
+    date                  DATE NOT NULL,
     warehouse_id          INT REFERENCES Warehouse,
+    shipping_box_id       INT REFERENCES ShippingBox,
+    medicine_id           INT REFERENCES Medicine,
+    pharmacy_id           INT REFERENCES Pharmacy,
     number_of_medications INT NOT NULL check ( number_of_medications >= 0 )
 );
-
 
 -- Принятие поставки
 CREATE TABLE Acceptance
 (
     car_id       INT REFERENCES Cars,
     warehouse_id INT REFERENCES Warehouse,
-    time         TIME NOT NULL,
-    date         DATE NOT NULL,
-    surname      TEXT NOT NULL,
-    UNIQUE (time, date, surname)
+    datetime     TIMESTAMP NOT NULL,
+    storekeeper_last_name      TEXT NOT NULL
 );
-
--- Поставки
-
 
 INSERT INTO Laboratory (name, head_last_name) VALUES ('HELIX', 'Иванов');
 INSERT INTO Certificate(laboratory_id, expiration_date) VALUES(1, '20-12-2022');
 INSERT INTO Distributor(address, bank_account_number, contact_first_name, contact_last_name, phone_number) VALUES ('Улица пушкина, дом колотушкина', '40817810570000123456', 'Иван', 'Иванов', '+79142231602');
 INSERT INTO Medicine(name) VALUES ('Парацетамол');
 INSERT INTO Medicine(name) VALUES ('Арбидол');
-INSERT INTO Pharmacy(address, number_people) VALUES('Аптека', '?');
+INSERT INTO Pharmacy(address, name) VALUES('Аптека', 'Аптека за углом №7');
 INSERT INTO Warehouse(address) VALUES ('Улица пушкина 1');
-INSERT INTO Supply(distributor_id, warehouse_id, arrival_date, arrival_time, arrival_storekeeper_surname) VALUES (1, 1, '20-11-2020', '20:30', 'Иванов');
+INSERT INTO Supply(distributor_id, warehouse_id, arrival_datetime, arrival_storekeeper_surname) VALUES (1, 1, '20-11-2020 20:30', 'Иванов');
